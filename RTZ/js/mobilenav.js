aa3d9f555535190ab85835aff019efc14537c6fd
Paul Szefer <pszefer@hotmail.com>
2017-05-03 16:10:59 -0700

    added animation of mobilenav when it appears

--- RTZ/js/mobilenav.js
+++ RTZ/js/mobilenav.js
@@ -2,20 +2,21 @@ $("#navburger").click(function() {
     if ($("#mobilenav").css("display") == "none") {
         $("#mobilenav").css("display", "block");
         $("#mobilenav").css("position", "absolute");
-        $("#mobilenav").css("left", 400);//$("window").width());
-        intervalID = window.setInterval(movenav, 200);
-        while (
+        $("#mobilenav").css("left", "400px");//$("window").width());
+        var intervalID = setInterval(movenav, 4);
+        
+        function movenav() {
+            var xPos = $("#mobilenav").css("left").slice(0, -2);
+            if (xPos > 0) {
+                xPos -= 2;
+                $("#mobilenav").css("left", xPos + "px");
+            }
+            
+            if ($("#mobilenav").css("left").slice(0, -2) <= 0) {
+                clearInterval(intervalID);
+            }
+        }
     } else {
         $("#mobilenav").css("display", "none");
     }
 });
-
-function movenav() {
-    var xPos = $("#mobilenav").css("left");
-    if (xPos > 0) {
-        xPos--;
-        $("#mobilenav").css("left", xPos);
-    } else {
-        window.clearInterval(intervalID);
-    }
-}
