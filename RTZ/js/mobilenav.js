47e69caa84ec0ee3dc333e3313d3acae74700f27
Paul Szefer <pszefer@hotmail.com>
2017-05-03 11:46:42 -0700

    mobile navbar togges

--- RTZ/js/mobilenav.js
+++ RTZ/js/mobilenav.js
@@ -1,10 +1,14 @@
 $("#navburger").click(function() {
-    $("#mobilenav").css("display", "block");
-    $("#mobilenav").css("position", "absolute");
-    $("#mobilenav").css("left", $("window").width());
-    var xPos = $("#mobilenav").css("left");
-    while (xPos > 0) {
-        xPos--;
-        $("#mobilenav").css("left", xPos);
+    if ($("#mobilenav").css("display") == "none") {
+        $("#mobilenav").css("display", "block");
+        $("#mobilenav").css("position", "absolute");
+        $("#mobilenav").css("left", $("window").width());
+        var xPos = $("#mobilenav").css("left");
+        while (xPos > 0) {
+            xPos--;
+            $("#mobilenav").css("left", xPos);
+        }
+    } else {
+        $("#mobilenav").css("display", "none");
     }
 });
