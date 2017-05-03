eea8adba1e1aaca303ba443927c5865e4cd29a1a
kttkjl <jinglun.li@alumni.ubc.ca>
2017-05-03 16:18:44 -0700

    added overlay and sidenav functionality, fixed js to run only after page is loaded

--- RTZ/js/mobilenav.js
+++ RTZ/js/mobilenav.js
@@ -1,9 +1,9 @@
 $("#navburger").click(function() {
     if ($("#mobilenav").css("display") == "none") {
         $("#mobilenav").css("display", "block");
-        $("#mobilenav").css("position", "absolute");
-        $("#mobilenav").css("left", $("window").width());
-        var xPos = $("#mobilenav").css("left");
+        $("#mobilenav").css("position", "fixed");
+        $("#mobilenav").css("right", $("window").width());
+        var xPos = $("#mobilenav").css("right");
         while (xPos > 0) {
             xPos--;
             $("#mobilenav").css("left", xPos);
