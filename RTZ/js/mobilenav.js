7cd441c146111dd59433319e970c466710062bc0
Paul Szefer <pszefer@hotmail.com>
2017-05-03 12:10:31 -0700

    start adding sliding mobilenav

--- RTZ/js/mobilenav.js
+++ RTZ/js/mobilenav.js
@@ -2,13 +2,20 @@ $("#navburger").click(function() {
     if ($("#mobilenav").css("display") == "none") {
         $("#mobilenav").css("display", "block");
         $("#mobilenav").css("position", "absolute");
-        $("#mobilenav").css("left", $("window").width());
-        var xPos = $("#mobilenav").css("left");
-        while (xPos > 0) {
-            xPos--;
-            $("#mobilenav").css("left", xPos);
-        }
+        $("#mobilenav").css("left", 400);//$("window").width());
+        intervalID = window.setInterval(movenav, 200);
+        while (
     } else {
         $("#mobilenav").css("display", "none");
     }
 });
+
+function movenav() {
+    var xPos = $("#mobilenav").css("left");
+    if (xPos > 0) {
+        xPos--;
+        $("#mobilenav").css("left", xPos);
+    } else {
+        window.clearInterval(intervalID);
+    }
+}
