0139ef0e0e0f127ad2e2e1e609a28de5d55098c9
Paul Szefer <pszefer@hotmail.com>
2017-05-06 14:48:47 -0700

    js for resource page topic show/hide complete

--- RTZ/js/topic.js
+++ RTZ/js/topic.js
@@ -4,22 +4,20 @@
 // hides all topics, and then shows the correct
 // maximized topic
 $(".topicmin").click(function() {
-    var count = 0;
-    if ($(".topicmin") == this) {
-        count++;
-        var id = count;
-    } else {
-        count++;
-    }
+    var index = $(".topicmin").index(this);
     hideTopics();
-    showTopic(id);
-}
+    $(".topicmax").get(index).style.display = "block"; // can animate later;
+});
+
+// adds a click handler for each maximized topic
+//
+// hides all topics, then shows all minimized topics
+$(".topicmax").click(function() {
+    hideTopics();
+    $(".topicmin").css("display", "block");
+});
 
 // hides all topics
 function hideTopics() {
     $(".topic").css("display", "none"); // can animate this instead later
-}
-
-function showTopic(id) {
-    $(".maxTopic")[id].css("display", "block"); // can animate later
 }
