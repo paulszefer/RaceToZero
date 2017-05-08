5b7bea6418b7dbd6801afb6100b0511c3a552441
Ian Younghusband <ianyounghusband@hotmail.com>
2017-05-08 11:19:06 -0700

    Actual Resources fix

--- RTZ/js/resource.js
+++ RTZ/js/resource.js
@@ -7,15 +7,11 @@ $(function() {
 $(".contentbox").click(function() {
     var index = $(".contentbox").index(this);
     var expanded = $(".expandedcontentbox").get(index);
-    var expanded2 = $(".contentwrap2").get(index);
     hideTopics();
     expanded.style.display = "block";
     expanded.style.minHeight = "100vh";
     expanded.style.height = "100%";
     expanded.style.width = "100%";
-    expanded2.style.display = "block";
-    expanded2.style.minHeight = "100vh";
-    expanded2.style.width = "100%";
 });
 
 // adds a click handler for each maximized topic
@@ -26,7 +22,6 @@ $(".expandedcontentbox").click(function() {
     //resets the inline attr caused by .css()
     $(".contentboxwrapper").attr('style', '');
     $(".contentbox").attr('style', '');
-    $(".expandedcontentbox").attr('style', '');
 });
 
 function hideTopics() {
