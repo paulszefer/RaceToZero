63d7eb4a63a3f43821f956ea1ffd128d9b6acc4c
Griffin Tench <griffintench@gmail.com>
2017-05-09 14:08:03 -0700

    Made getbarriers.php

--- RTZ/getbarriers.php
+++ RTZ/getbarriers.php
@@ -2,18 +2,31 @@
 
 	require_once('PDO_conn.php');
 	
-	$level_id = $_POST['level_id'];
+	function getBarriers() {
 	
-	try {
-		$statement = $DB_conn->prepare("SELECT barrier_name, 
-										       barrier_x1,
-										       barrier_y1,
-										       barrier_x2,
-										       barrier_y2
-										FROM barriers
-										WHERE level_id='" . $level_id . "';");
-	} catch(PDOException $e) {
-	  	echo $e->getMessage();
+		$level_id = $_POST['level_id'];
+	
+		$barriers = array();
+	
+		try {
+			$statement = $DB_conn->prepare("SELECT barrier_name, 
+											       barrier_x1,
+											       barrier_y1,
+											       barrier_x2,
+											       barrier_y2
+											FROM barriers
+											WHERE level_id='" . $level_id . "';");
+			$statement->execute();
+			while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+				$barriers[] = $row;
+			}
+		} catch(PDOException $e) {
+	  		echo $e->getMessage();
+		}
+	
+		$json = json_encode($barriers);
+		return $json;
+	
 	}
 
 ?>
