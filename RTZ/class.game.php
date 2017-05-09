c139c9ca487fa8a537dd435ef5e3891fc2a862a5
Griffin Tench <griffintench@gmail.com>
2017-05-09 10:57:52 -0700

    Created game save functionality in PHP

---
+++ RTZ/class.game.php
@@ -0,0 +1,46 @@
+<?php
+
+	require_once('PDO_conn.php');
+	
+	class GAME {
+		
+		private $conn;
+		private $uname;
+		private $level;
+		private $time;
+		
+		public function __construct($DB_conn, $uname, $level, $time) {
+			$this->conn = $DB_conn;
+			$this->uname = $uname;
+			$this->level = $level;
+			$this->time = $time;
+		}
+		
+		public function save() {
+			try {
+				$stmt1 = $this->conn->prepare(
+					"SELECT user_id
+					FROM games
+					WHERE user_name='" . $this->uname . "';";
+				$stmt1->execute();
+				$row = $statement->fetch(PDO::FETCH_ASSOC);
+				
+				$userid = $row['user_id'];
+			
+				$stmt = $this->conn->prepare(
+					"INSERT INTO games(game_level,game_time,user_id) 
+					VALUES(:level, :time, :userid)");
+				$stmt->bindparam(":level", $this->level);
+				$stmt->bindparam(":time", $this->time);
+				$stmt->bindparam(":userid", $userid);
+				$stmt->execute();
+				return $stmt;	
+			}
+			catch(PDOException $e) {
+				echo $e->getMessage();
+			}
+		}
+		
+	}
+
+?>
