a0cba94936c9c8add38cc452b37f585d7d0f8fe4
Griffin Tench <griffintench@gmail.com>
2017-05-11 14:18:06 -0700

    Leaderboard working better

--- RTZ/profile.php
+++ RTZ/profile.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('PDO_conn.php');
+	/*require_once('PDO_conn.php');
 	
 	function displayTutorialScore() {
 		$username = "root";
@@ -22,75 +22,34 @@
 		}
 		
 		return $$row['game_time'];
-	}
+	}*/
 ?>
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<link rel="stylesheet" href="css/masterreset.css">
-		<link rel="stylesheet" href="css/template2.css">
-        <link rel="stylesheet" href="css/profile.css">
-		<script src='js/template.js'></script>
-		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-        <script src="js/overlaynav.js"></script>
-	</head>
-	</html>
-	<body>
-		<div id=overlay></div>
-		<div id="header">
-			<div id='navCont'>
-				<div class='navlogo'>
-					<a href='index.html'><img src='img/logo.png' alt='logo'></a>
-					<div class='loginstatus'>
-						<p>Welcome, Horse</p>
-					</div>
-					<div id='navburger'>
-						<img src='img/menuicon.png'>
-					</div>
-				</div> <!--end of navlogo-->
-				<div class='navlinks'>
-					<ul>
-						<a href='index.html'><li class='navbuttonleft'>Home</li></a>
-						<a href='resource.html'><li class='navbuttonleft'>Resources</li></a>
-						<a href='login.html'><li class='navbuttonright'>Login/Register</li></a>
-						<a href='profile.html'><li class='navbuttonright'>Profile</li></a>
-						<a href='leaderboard.html'><li class='navbuttonleft'>Leaderboard</li></a>
-					</ul>
-				</div><!--end ov navlinks-->
 
-	            <div id="mobilenav">
-	                <ul>
-	                <a href='index.html'><li class='mobilelist'><div>Home</div></li></a>
-					<a href='resource.html'><li class='mobilelist'><div>Resources</div></li></a>
-					<a href='login.html'><li class='mobilelist'><div>Login/Register</div></li></a>
-					<a href='profile.html'><li class='mobilelist'><div>Profile</div></li></a>
-					<a href='leaderboard.html'><li class='mobilelist'><div>Leaderboard</div></li></a>
-	                </ul>
-	            </div><!--end of mobilenav-->
+<?php include("templateHeader.php");?>
+<link rel="stylesheet" href="css/profile.css">
+<?php include("templateNav.php");?>
 
-			</div>
-		</div>	
-		<div id='content'>
-			<div class='contentactual'>
-				<h1>Profile</h1>
-				<br>
-                <img src='img/setprofilepic.png'>
-				<ul>
-                    <li>Username: Huehue</li>
-                    <li>&nbsp;</li>
-                    <li>High score: <?php displayTutorialScore() ?></li>
-                    <li>&nbsp;</li>
-                    <li>Something: helloitsme</li>
-                </ul>
-                <br>
-			</div>
-		</div>
-		<div id='footer'>
-			<div class='footercontent'>
-				<p>Copyright whatever</p>
-			</div>
-		</div>
+<div id='content'>
+	<div class='contentactual'>
+		<h1>Profile</h1>
+		<br>
+        <img src='img/setprofilepic.png'>
+		<ul>
+            <li>Username: Huehue</li>
+            <li>&nbsp;</li>
+            <li>High score: <?php displayTutorialScore() ?></li>
+            <li>&nbsp;</li>
+            <li>Something: helloitsme</li>
+        </ul>
+        <br>
+	</div>
+</div>
+<div id='footer'>
+	<div class='footercontent'>
+		<p>Copyright whatever</p>
+	</div>
+</div>
 
-	</body>
+</body>
 
 </html>
