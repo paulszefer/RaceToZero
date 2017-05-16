74ee8808d37c5ddda14b2782fbadada2e89af05f
Griffin Tench <griffintench@gmail.com>
2017-05-16 16:49:28 -0700

    resolved conflict

--- RTZ/PDO_conn.php
+++ RTZ/PDO_conn.php
@@ -4,25 +4,12 @@
 // instead.
 session_start();
 
-$debugMode = true;
+//for localhost testing
 
-if ($debugMode) {
-    $DB_host = "localhost";
-    $DB_user = "root";
-    $DB_pass = "";
-    $DB_name = "comp2910test1";
-} else {
-    $DB_host = "sql201.byethost12.com";
-    $DB_user = "b12_20107247";
-    $DB_pass = "password.1";
-    $DB_name = "b12_20107247_ZeroSquad2";
-}
-
-//for official host
-// $DB_host = "sql201.byethost12.com";
-// $DB_user = "b12_20107247";
-// $DB_pass = "password.1";
-// $DB_name = "b12_20107247_ZeroSquad2";
+$DB_host = "localhost";
+$DB_user = "root";
+$DB_pass = "";
+$DB_name = "comp2910test1";
 
 
 // $DB_host = 'bcitdevcom.ipagemysql.com';
