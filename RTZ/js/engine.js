65d007f0caa798fec722246d49667978b0572ec2
Paul Szefer <pszefer@hotmail.com>
2017-05-17 11:21:53 -0700

    disable double tapping zoom on mobile

--- RTZ/js/engine.js
+++ RTZ/js/engine.js
@@ -656,6 +656,23 @@ $(function () {
             // setTimeout(setClicked, 500, false);
         }
     });
+
+    $.fn.nodoubletapzoom = function() {
+        $(this).bind('touchstart', function preventZoom(e) {
+            var t2 = e.timeStamp;
+            var t1 = $(this).data('lastTouch') || t2;
+            var dt = t2 - t1;
+            var fingers = e.originalEvent.touches.length;
+            $(this).data('lastTouch', t2);
+            if (!dt || dt > 300 || fingers > 1) {
+                return; // not double-tap
+            }
+            e.preventDefault(); // double tap - prevent the zoom
+            // also synthesize click events we just swallowed up
+            $(e.target).trigger('click');
+        });
+    };
+    $('#content').nodoubletapzoom();
 });
 
 /**
