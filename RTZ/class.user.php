026b357b28c20f596d2d77015ade60682f4c3157
Jacky Li <jinglun.li@alumni.ubc.ca>
2017-05-19 02:46:43 -0700

    revamped login.php, added all functionalities in class.user.php

--- RTZ/class.user.php
+++ RTZ/class.user.php
@@ -6,31 +6,67 @@ class USER {
 	private $conn;
 	private $myId;
 	private $loggedIn;
+	public $loginError;
+	public $registerError;
 	
 	public function __construct($DB_conn) {
 		//I'm assuming $this->conn is USER.conn (java)
 		$this->conn = $DB_conn;
+		if (isset($_SESSION['user_session'])){
+			$this->loggedIn = true;
+			$this->myId = $_SESSION['user_id'];
+		}
     }
 	
 	//Sets up registration function
 	public function register($uname,$upass,$photourl) {
-		try {
-			$stmt = $this->conn->prepare(
-				"INSERT INTO users(user_name,user_password,user_photo) 
-				VALUES(:uname, :upass, :photourl)");
-			$stmt->bindparam(":uname", $uname);
-			$stmt->bindparam(":upass", $upass);
-			$stmt->bindparam(":photourl", $photourl);
-			$stmt->execute();
-			return $stmt;	
-		}
-		catch(PDOException $e) {
-			echo $e->getMessage();
-		}
-	}
+		$this->registerError = '';
+		$regUserPattern = '/^[a-zA-Z0-9]{4,16}(?!.{1,})$/';
+		$regPassPattern = '/^[a-zA-Z0-9]{6,}$/';
+		//Check if they entered username and password
+		if(empty($uname)||empty($upass)){
+			$this->registerError='No username or password provided';
+			return false;
+		} else if (!preg_match($regUserPattern, $uname)){
+			//check if they entered the right user pattern
+			$this->registerError='Username did not match specified pattern';
+			return false;
+		} else if (!preg_match($regPassPattern, $upass)){
+			//check if they entered the right password pattern
+			$this->registerError='Password contains illegal characters';
+			return false;
+		} else {
+			//if they passed all of these tests, check if username already taken
+			try {
+				$statement = $this->conn->prepare(
+					"SELECT user_name
+					FROM users 
+					WHERE user_name=:uname");
+				$statement->execute(array(':uname'=>$uname));
+				$row = $statement->fetch(PDO::FETCH_ASSOC);
+				//Check if username is already taken
+				if ($row['user_name'] == $uname) {
+					$this->registerError = 'Username already taken';
+					return false;
+				} else {
+					//If they passed all the tests, register them in
+					$stmt = $this->conn->prepare(
+					"INSERT INTO users(user_name,user_password,user_photo) 
+					VALUES(:uname, :upass, :photourl)");
+					$stmt->execute(array(':uname'=>$uname, ':upass'=>$upass,':photourl'=>$photourl));
+					//and log them in automatically
+					$this->doLogin($uname, $upass);
+					//and redirect them to login script
+					$this->redirect('login.php');
+				}//End of else
+			} catch(PDOException $e) {
+				echo $e->getMessage();
+		}//End of catch for register
+	}} //End of Register
 
 	//Returns true or false if username/email is in DB, then check password
 	public function doLogin($uname,$upass) {
+		$this->loginError ='';
 		try {
 			$stmt = $this->conn->prepare(
 				"SELECT user_name, user_password, user_id
@@ -45,8 +81,6 @@ class USER {
 				if($upass == $userRow['user_password']) {
 					//sets the $_SESSION array at 'user_session' as id grabbed from DB table users
 					$this->myId = $userRow['user_id'];
-					//$_SESSION['user_session'] = $userRow['id'];
-					//temp ghetto fix atm
 					$_SESSION['user_session'] = $userRow['user_name'];
 					$_SESSION['user_name'] = $userRow['user_name'];
 					$_SESSION['user_id'] = $userRow['user_id'];
@@ -57,19 +91,28 @@ class USER {
 				}
 				else {
 					$this->loggedIn = false;
+					$this->loginError = 'Incorrect Password';
 					return false;
 				}
+			} else {
+				$this->loginError = 'Username does not exist';
+				return false;
 			}
 		}
 		catch(PDOException $e) {
 			echo $e->getMessage();
 		}
 	}
-	
+	public function displayLoginError(){
+		return $this->loginError;
+	}
+	public function displayRegError(){
+		return $this->registerError;
+	}
 	//checks if an user object is logged in
 	public function is_loggedin() {
 		//isset() just determines if a var isn't null - 'user_session' is key, check if NULL
-		return isset($_SESSION['user_session']);
+		return $this->loggedIn;
 	}
 	
 	//Redirects user to another page
