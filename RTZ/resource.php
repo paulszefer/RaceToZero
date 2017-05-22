1673132c381ba629647c711932c82d5b724ed01f
Ian Younghusband <ianyounghusband@hotmail.com>
2017-05-22 13:09:14 -0700

    fixed mobile icons, resources aff apps alignment, and resources xbutton padding

--- RTZ/resource.php
+++ RTZ/resource.php
@@ -188,7 +188,7 @@
 			<div class='expandedcontentbox bigexpandedcontentbox'>	<!-- Affiliated Apps -->
 				<img class="xbutton" id="bigone" src="img/tempX.png">
 				<h1>AFFILIATED APPS</h1>
-				<p>
+				<p id=centeraffappstext>
 				Here are some other apps that we have connection with!
 				</p>
 				<br>
