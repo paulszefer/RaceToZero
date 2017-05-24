192cf31781d3701ae9142d8c61e31230a5cb0a03
Jacky Li <jinglun.li@alumni.ubc.ca>
2017-05-24 11:04:07 -0700

    added twitter autoBot, credentials are in our secret secret php file shhhhh

--- RTZ/class.game.php
+++ RTZ/class.game.php
@@ -24,7 +24,35 @@
     		$result = mysqli_query($link, $userIDQuery);
     		$row = mysqli_fetch_array($result);
     		$userid = intval($row['user_id']);
-    			
+    		
+    		//Grabs the current level 3 highest scores
+    		$topScoreQuery = "SELECT game_time, user_name
+    		FROM games
+    		WHERE game_level = " . $this->level . " ORDER BY game_time ASC
+    		LIMIT 3;";
+    		$topScoreResult = mysqli_query($link, $topScoreQuery);
+
+    		//Check if score is smaller than the fetched rows
+			if($topScoreResult) {
+				$resultRow;
+				$shouldPost = false;
+				while($resultRow = mysqli_fetch_array($topScoreResult)) {
+					//forces int onto result row, at index 0 - being game_time
+					if($this->time < intval($resultRow['game_time'])){
+						$shouldPost = true;
+					}
+				}
+				if ($shouldPost) {
+					//if at anytime new score is better, call for twitter post
+					require_once('twitterOAuth.php');
+					//Prepares a message to send to twitter
+					$status = 'Congrats! '. $resultRow['user_name'] . ' has a new score on level '
+					. $this->level . ' with a time of: '.(($this->time)/1000).' seconds!';
+					//Posts via twitter
+					$connection->post('statuses/update', array('status'=>$status));
+				}
+			}
+			
     		$query = "INSERT INTO games(level_id,game_time,user_id) 
 					  VALUES(" . $this->level . ", " . $this->time . ", " . $userid . ");";
 			mysqli_query($link, $query);
