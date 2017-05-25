388f50d800ad4b34671faf64c62589fee56c7203
Paul Szefer <pszefer@hotmail.com>
2017-05-25 13:52:48 -0700

    about page styling

--- RTZ/about.php
+++ RTZ/about.php
@@ -7,18 +7,20 @@
 ?>
 
 <?php include("templateHeader.php");?>
+<link rel="stylesheet" href="css/about.css">
 <?php include("templateNav.php");?>
 
 <title>Race to Zero - About</title>
 <div id='content'>
     <div class='contentactual'>
         <h1>About</h1>
-        <p>Check out the official <a href="https://twitter.com/RaceToZero2017">Race to Zero Twitter account!</a></p>
-        <p>In-game music is thanks to BENSOUND.</p>
-        <p>Music by BENSOUND <a href="http://www.bensound.com/royalty-free-music">http://www.bensound.com/royalty-free-music</a></p>
-        <p>Creative Commons -- Attribution 3.0 Unported -- CC BY 3.0</p>
-        <p><a href="http://creativecommons.org/licenses/by/3.0/">http://creativecommons.org/licenses/by/3.0/</a></p>
-        <p>Music provided by Audio Library <a href="https://goo.gl/YJz9BK">https://goo.gl/YJz9BK</a></p>
+        <br>
+        <p class="aboutParagraph">Check out the official <a href="https://twitter.com/RaceToZero2017">Race to Zero Twitter account!</a></p>
+        <p class="aboutParagraph">In-game music is thanks to BENSOUND.</p>
+        <p class="aboutParagraph">Music by BENSOUND <a href="http://www.bensound.com/royalty-free-music">http://www.bensound.com/royalty-free-music</a></p>
+        <p class="aboutParagraph">Creative Commons -- Attribution 3.0 Unported -- CC BY 3.0</p>
+        <p class="aboutParagraph"><a href="http://creativecommons.org/licenses/by/3.0/">http://creativecommons.org/licenses/by/3.0/</a></p>
+        <p class="aboutParagraph">Music provided by Audio Library <a href="https://goo.gl/YJz9BK">https://goo.gl/YJz9BK</a></p>
     </div>
 </div>
 <?php include("templateFooter.php");?>
